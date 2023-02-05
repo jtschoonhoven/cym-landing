74fdc88e0807d8874031b0424175c371eba8ec1f
jtschoonhoven <jtschoonhoven@gmail.com>
2023-02-04 16:36:29 -0800

    Improve email signup styles

--- src/components/email-signup/EmailSignup.tsx
+++ src/components/email-signup/EmailSignup.tsx
@@ -1,59 +1,76 @@
+import { Box, Button, FormControl, FormHelperText, InputAdornment, OutlinedInput } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import React from 'react';
+import { HTMLAttributes } from 'react';
 
 interface Props {
   title?: React.ReactElement;
+  color?: HTMLAttributes<HTMLInputElement>['color'];
 }
 
 /**
  * An embedded Mailchimp email signup form.
  */
-const EmailSignup: React.FC<Props> = ({ title }) => {
+const EmailSignup: React.FC<Props> = ({ title, color }) => {
+  color = color || 'black';
   return (
     <div id="mc_embed_signup">
-      <form
+      <Box
+        component="form"
         action="https://show.us13.list-manage.com/subscribe/post?u=dcef62d2baadac35b443af1c9&amp;id=44ba8a8363&amp;f_id=0088e1e2f0"
         method="post"
         id="mc-embedded-subscribe-form"
         name="mc-embedded-subscribe-form"
-        className="validate"
         target="_blank"
         style={{ margin: 0, padding: 0 }}
         noValidate
       >
-        <div id="mc_embed_signup_scroll">
-          {title ? <>{title}</> : <h2 color="black">Subscribe for email updates</h2>}
-          <div className="mc-field-group">
-            {/* <label htmlFor="mce-EMAIL">Email</label> */}
-            <input
-              type="email"
-              name="EMAIL"
-              className="required email"
-              id="mce-EMAIL"
-              placeholder="your.email@domain.com"
-              required
-            />
-            <span id="mce-EMAIL-HELPERTEXT" className="helper_text" style={{ backgroundColor: 'inherit' }}></span>
-          </div>
-          <div id="mce-responses" className="clear foot">
-            <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
-            <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
-          </div>
-          <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
-            <input type="text" name="b_dcef62d2baadac35b443af1c9_44ba8a8363" tabIndex={-1} value="" />
-          </div>
-          <div className="optionalParent">
-            <div className="clear foot">
-              <input
-                type="submit"
-                defaultValue="Subscribe"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                className="button"
-              />
-            </div>
-          </div>
-        </div>
-      </form>
+        <Box id="mc_embed_signup_scroll">
+          {title ? <>{title}</> : <h2 color={color}>Subscribe for email updates</h2>}
+          <Grid container flexDirection="row" spacing={0}>
+            <Grid xs={12}>
+              <FormControl style={{ padding: 0 }}>
+                <OutlinedInput
+                  type="email"
+                  name="EMAIL"
+                  id="mce-EMAIL"
+                  placeholder="email@domain.com"
+                  aria-describedby="mce-EMAIL-HELPERTEXT"
+                  sx={{ color, padding: 0 }}
+                  endAdornment={
+                    <InputAdornment position="end" style={{ width: '200px' }}>
+                      <Button
+                        variant="contained"
+                        type="submit"
+                        size="large"
+                        name="subscribe"
+                        id="mc-embedded-subscribe"
+                        style={{ marginLeft: 0 }}
+                      >
+                        Subscribe
+                      </Button>
+                    </InputAdornment>
+                  }
+                  inputProps={{ color }}
+                  required
+                />
+              </FormControl>
+            </Grid>
+            <Grid>
+              <div id="mce-responses">
+                <div id="mce-error-response" style={{ display: 'none' }}></div>
+                <div id="mce-success-response" style={{ display: 'none' }}></div>
+              </div>
+              <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+                <input type="text" name="b_dcef62d2baadac35b443af1c9_44ba8a8363" tabIndex={-1} value="" />
+              </div>
+            </Grid>
+            <Grid xs={12}>
+              <FormHelperText id="mce-EMAIL-HELPERTEXT"></FormHelperText>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </div>
   );
 };
--- src/components/no-sleep/NoSleepLanding.tsx
+++ src/components/no-sleep/NoSleepLanding.tsx
@@ -1,12 +1,12 @@
-import { Box, Container, Dialog, Slide, useTheme } from '@mui/material';
+import { Container, Dialog, Slide } from '@mui/material';
 import React from 'react';
 import { TransitionProps } from '@mui/material/transitions';
 import EmailSignup from '../email-signup/EmailSignup';
 import Subtitle1 from '../typography/Subtitle1';
-import HeroTitle from '../hero/HeroTitle';
 import wheelIcon from '../../assets/images/wheel-icon.png';
 import cymLogo from '../../assets/images/cym-title.png';
 import Grid from '@mui/material/Unstable_Grid2';
+import CloseIcon from '@mui/icons-material/Close';
 
 /**
  * A MUI transition component.
@@ -52,41 +52,49 @@ const NoSleepLanding: React.FC = () => {
         TransitionComponent={Transition}
         PaperProps={{ sx: { height: '100%', backgroundColor: 'black' } }}
       >
+        <CloseIcon
+          sx={{ right: 0, color: 'white', position: 'absolute', marginTop: 2, marginRight: 2, fontSize: '3rem' }}
+          onClick={onClose}
+        />
         <Container maxWidth="xs" sx={{ height: '100%', marginTop: 5 }}>
-          <Grid container spacing={1}>
-            <Grid
-              xs={6}
-              sx={{
-                minHeight: '200px',
-                height: '20vh',
-                backgroundImage: `url("${cymLogo}")`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'right',
-                backgroundSize: 'contain',
-              }}
-            />
-            <Grid
-              xs={6}
-              sx={{
-                minHeight: '200px',
-                height: '20vh',
-                backgroundImage: `url("${wheelIcon}")`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'right',
-                backgroundSize: 'contain',
-              }}
-            />
+          <Grid container justifyContent="center" flexDirection="column" sx={{ height: '100%' }}>
+            <Grid container sx={{ minHeight: '200px', height: '20vh', width: '100%' }}>
+              <Grid
+                xs={6}
+                sx={{
+                  minHeight: '200px',
+                  height: '20vh',
+                  backgroundImage: `url("${cymLogo}")`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'right',
+                  backgroundSize: 'contain',
+                }}
+              />
+              <Grid
+                xs={6}
+                sx={{
+                  minHeight: '200px',
+                  height: '20vh',
+                  backgroundImage: `url("${wheelIcon}")`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'right',
+                  backgroundSize: 'contain',
+                }}
+              />
+            </Grid>
+            <Grid xs={12}>
+              <EmailSignup
+                title={
+                  <>
+                    <Subtitle1 sx={{ marginBottom: 2 }}>
+                      Change Your Mind is an immersive show coming to San Francisco in April. Subscribe to be notified
+                      when tickets go on sale!
+                    </Subtitle1>
+                  </>
+                }
+              />
+            </Grid>
           </Grid>
-          <EmailSignup
-            title={
-              <>
-                <Subtitle1 sx={{ marginBottom: 2 }}>
-                  Change Your Mind is an immersive show coming to San Francisco in April. Subscribe to be notified when
-                  tickets go on sale!
-                </Subtitle1>
-              </>
-            }
-          />
         </Container>
       </Dialog>
     </>
