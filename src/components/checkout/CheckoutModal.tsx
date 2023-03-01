9f3892cb05ed5d7682b05afc5c98af62696299ef
jtschoonhoven <jtschoonhoven@gmail.com>
2023-03-01 10:43:42 -1000

    Update content warnings

--- src/components/checkout/CheckoutModal.tsx
+++ src/components/checkout/CheckoutModal.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Alert, AlertTitle, Box, Button, Checkbox, Container, Dialog, Typography } from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  AlertTitle,
+  Box,
+  Container,
+  Dialog,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import { CheckoutContext } from '../../providers/CheckoutProvider';
 import Bold from '../typography/Bold';
-import EmailSignup from '../email-signup/EmailSignup';
+import { ExpandMore } from '@mui/icons-material';
 
 export const CHECKOUT_MODAL_CONTAINER_ID = 'checkout-modal';
 export const CHECKOUT_MODAL_CONTAINER_HEIGHT_PX = 500;
@@ -12,7 +22,8 @@ export const CHECKOUT_MODAL_CONTAINER_HEIGHT_PX = 500;
  */
 const CheckoutModal: React.FC = () => {
   const { isOpen, setIsOpen } = React.useContext(CheckoutContext);
-  // const [isConfirmed, setIsConfirmed] = React.useState(false);
+  const [isConfirmed, setIsConfirmed] = React.useState(false);
+  const theme = useTheme();
 
   return (
     <>
@@ -22,37 +33,31 @@ const CheckoutModal: React.FC = () => {
             Tickets available now!
           </Alert> */}
           {/* <EmailSignup /> */}
-          <Alert severity="error" sx={{ justifyContent: 'center' }}>
-            {/* <Box style={{ display: isConfirmed ? 'none' : 'block' }}> */}
-            <AlertTitle>
-              <Bold>Warning! This performance includes the following:</Bold>
-            </AlertTitle>
-            <ul>
-              <li>Loud noises</li>
-              <li>Bright and flashing lights</li>
-              <li>Themes of mental illness and hospitalization</li>
-              <li>Movement that may be challenging for people with limited mobility (most wheelchairs OK)</li>
-              <li>Sight and hearing restricted by headphones and goggles</li>
-            </ul>
-            <Typography variant="body2">
-              That said, this is not a "scary" experience and we hope you leave feeling inspired.
-            </Typography>
-            <br />
-            {/* </Box> */}
-            <Bold>
-              By purchasing a ticket I confirm I am at least 18 years old and I am aware of the content warnings.
-            </Bold>
-            <br />
-            {/* <Button
-              variant="contained"
-              color="warning"
-              onClick={() => setIsConfirmed(!isConfirmed)}
-              sx={{ width: '100%' }}
-            >
-              <Checkbox checked={isConfirmed} color="default" onChange={() => setIsConfirmed(!isConfirmed)} />{' '}
-              <Bold>{isConfirmed ? 'Confirmed' : 'Confirm and select tickets'}</Bold>
-            </Button> */}
-          </Alert>
+          <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMore />}>
+              <Bold>
+                By purchasing a ticket you confirm that you are at least 18 years old and have read the content
+                warnings.
+              </Bold>
+            </AccordionSummary>
+            <AccordionDetails>
+              <AlertTitle>
+                <Bold>Please note, this show includes the following:</Bold>
+              </AlertTitle>
+              <ul>
+                <li>Loud noises</li>
+                <li>Bright and flashing lights</li>
+                <li>Themes of mental illness and hospitalization</li>
+                <li>Movement that may be challenging for people with limited mobility (most wheelchairs OK)</li>
+                <li>Sight and hearing restricted by headphones and goggles</li>
+              </ul>
+              <Typography variant="body2">
+                <Bold>That said, this is not a "scary" experience and we hope you leave feeling inspired!</Bold>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <br />
+          <br />
         </Container>
         <Box
           id={CHECKOUT_MODAL_CONTAINER_ID}
