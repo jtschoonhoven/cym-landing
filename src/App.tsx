81a84aef044fd141cd73ca3c56316a243a6a0e26
Alex Howard <alex@howardsupply.com>
2023-03-06 12:33:22 -0800

    add useScrollToLocation for onload

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,35 @@ const AppWrapper = styled(Paper)`
   padding-bottom: 30px;
 `;
 
+const useScrollToLocation = () => {
+  const scrolledRef = React.useRef(false);
+  const { hash } = window.location;
+  const hashRef = React.useRef(hash);
+
+  React.useEffect(() => {
+    if (hash) {
+      // We want to reset if the hash has changed
+      if (hashRef.current !== hash) {
+        hashRef.current = hash;
+        scrolledRef.current = false;
+      }
+
+      // only attempt to scroll if we haven't yet (this could have just reset above if hash changed)
+      if (!scrolledRef.current) {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+          scrolledRef.current = true;
+        }
+      }
+    }
+  });
+};
+
 const App: React.FC = () => {
+  useScrollToLocation();
+
   return (
     <>
       <CheckoutModal />
