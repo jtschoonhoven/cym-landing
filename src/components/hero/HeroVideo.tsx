60646e8ef95cee00bb34064ee33fe1f7f663ab1c
jtschoonhoven <jtschoonhoven@gmail.com>
2023-03-06 21:40:31 -1000

    Add new teaser to header

--- src/components/hero/HeroVideo.tsx
+++ src/components/hero/HeroVideo.tsx
@@ -1,6 +1,8 @@
-import { Box } from '@mui/material';
+import { Box, Button, IconButton, Typography } from '@mui/material';
 import React, { PropsWithChildren } from 'react';
-import videoSrc from '../../assets/video/teaser_md.mp4';
+import teaserShort from '../../assets/video/teaser-wide-short.mp4';
+import teaserRepeat from '../../assets/video/teaser-wide-repeat.mp4';
+import { VolumeDown, VolumeOff, VolumeUp } from '@mui/icons-material';
 
 interface Props extends PropsWithChildren {
   height: number;
@@ -13,6 +15,7 @@ interface Props extends PropsWithChildren {
 const HeroVideo: React.FC<Props> = ({ height, initialWidthPx, children }) => {
   const [width, setWidth] = React.useState(initialWidthPx || window.outerWidth);
   const containerRef = React.createRef<HTMLDivElement>();
+  const [isMuted, setMuted] = React.useState(false);
 
   // Watch for resize events and change the video width
   React.useEffect(() => {
@@ -31,20 +34,29 @@ const HeroVideo: React.FC<Props> = ({ height, initialWidthPx, children }) => {
 
   return (
     <Box ref={containerRef}>
+      <Button
+        variant="outlined"
+        endIcon={isMuted ? <VolumeUp /> : <VolumeOff />}
+        color="primary"
+        sx={{ float: 'right', position: 'absolute', right: 30, top: 690, zIndex: 3, color: 'white' }}
+        onClick={() => setMuted(!isMuted)}
+      >
+        {isMuted ? 'Unmute' : 'Mute'}
+      </Button>
       <Box sx={{ position: 'absolute', float: 'left', zIndex: 1, width: '100%' }}>{children}</Box>
       <video
-        src={videoSrc}
+        src={teaserShort}
         style={{
           filter: 'grayscale(50%) brightness(50%)',
           display: 'block',
           objectFit: 'cover',
-          objectPosition: '50% 95%',
+          objectPosition: '50% 50%',
           height,
           width,
         }}
         autoPlay
         loop
-        muted
+        muted={isMuted}
       />
     </Box>
   );
