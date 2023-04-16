a5ca6c739ee903eaadb54c6994d5721412cca437
jtschoonhoven <jtschoonhoven@gmail.com>
2023-04-16 13:34:59 -0700

    Add credits

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -4,11 +4,11 @@ import axonImg from '../../assets/images/axon.jpg';
 import HeroImage from '../util/hero-image/HeroImage';
 import AboutSteps from './AboutSteps';
 import AboutPress from './AboutPress';
-import AboutIcons from './AboutIcons';
 import AboutBannerPrimary from './AboutBannerPrimary';
 import AboutBannerSecondary from './AboutBannerSecondary';
 import AboutStaff from './AboutStaff';
 import AboutFAQs, { ABOUT_FAQS_PRIMARY_ID } from './AboutFAQs';
+import AboutCredits from './AboutCredits';
 
 /**
  * The primary component for below-the-fold content.
@@ -22,13 +22,19 @@ const About: React.FC = () => {
       <AboutBannerSecondary />
       {/* <Container maxWidth="lg" sx={{ marginTop: 5 }} children={<AboutIcons />} /> */}
       <Container maxWidth="lg" sx={{ marginTop: 5 }} children={<AboutStaff />} />
-      <Container id={ABOUT_FAQS_PRIMARY_ID} maxWidth="lg" sx={{ marginTop: 5, paddingTop: 5, borderTop:`30px solid ${theme.palette.secondary.dark}` }} children={<AboutFAQs />} />
+      <Container
+        id={ABOUT_FAQS_PRIMARY_ID}
+        maxWidth="lg"
+        sx={{ marginTop: 5, paddingTop: 5, borderTop: `30px solid ${theme.palette.secondary.dark}` }}
+        children={<AboutFAQs />}
+      />
       <HeroImage
         src={axonImg}
         sx={{ marginTop: 4 }}
         borderTop={`30px solid ${theme.palette.secondary.dark}`}
         borderBottom={`30px solid ${theme.palette.grey[700]}`}
       />
+      <Container maxWidth="lg" sx={{ marginTop: 5 }} children={<AboutCredits />} />
       <Container maxWidth="lg" sx={{ marginTop: 5 }} children={<AboutPress />} />
     </>
   );
---
+++ src/components/about/AboutCredits.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Typography, Unstable_Grid2 as Grid, styled } from '@mui/material';
+import H2 from '../typography/H2';
+import '@fontsource/old-standard-tt';
+
+const Bold = styled(Typography)({ textTransform: 'uppercase', display: 'inline', fontWeight: 700 });
+const Italic = styled(Typography)({ fontFamily: 'Old Standard TT, serif', fontStyle: 'italic', display: 'inline' });
+const BoldItalic = styled(Italic)({ fontWeight: 700 });
+
+const AboutCredits: React.FC = () => {
+  return (
+    <>
+      <H2 sx={{ marginBottom: 0 }}>Credits</H2>
+      <Grid container spacing={3} marginTop={1}>
+        <Grid xs={12} textAlign="right">
+          <BoldItalic>Directed by Jonathan Schoonhoven & Ellie DiBerardino</BoldItalic>
+          <br />
+          <BoldItalic>Written by Jonathan Schoonhoven</BoldItalic>
+          <br />
+          <BoldItalic>Technical Design & Fabrication by Alex Howard</BoldItalic>
+          <br />
+          <BoldItalic>A production by Say Nothing and Leave</BoldItalic>
+        </Grid>
+        <Grid md={6} sm={12}>
+          <Bold>Audio & Music: </Bold>
+          <Italic>Ellie DiBerardino</Italic>
+          <br />
+          <Bold>Videos: </Bold>
+          <Italic>Bryan Bigler, Ellie DiBerardino</Italic>
+          <br />
+          <Bold>Visual Design: </Bold>
+          <Italic>Jonathan Schoonhoven, Netta Schwarz</Italic>
+          <br />
+          <Bold>Marketing: </Bold>
+          <Italic>S. Caldwell</Italic>
+          <br />
+          <Bold>Set Design: </Bold>
+          <Italic>Andrew Somerville, Alyssa Larson</Italic>
+          <br />
+          <Bold>Booth Design & Build: </Bold>
+          <Italic>Jonathan Schoonhoven, Carson Auld</Italic>
+          <br />
+          <Bold>Diagnosis Tool: </Bold>
+          <Italic>Ellie DiBerardino, Alex Howard</Italic>
+          <br />
+          <Bold>Technical Support: </Bold>
+          <Italic>Dave Kaplan</Italic>
+        </Grid>
+        <Grid md={6} sm={12}>
+          <Bold>Dr. Masc: </Bold>
+          <Italic>L.M. Bogad</Italic>
+          <br />
+          <Bold>Rogue Technician: </Bold>
+          <Italic>Lily Lamboy</Italic>
+          <br />
+          <Bold>Production Support: </Bold>
+          <Italic>Elise Liu</Italic>
+          <br />
+          <Bold>Voices: </Bold>
+          <Italic>S. Caldwell, Jonathan Schoonhoven, Lou DiBerardino, Dan Gerbang, Ellie DiBerardino</Italic>
+          <br />
+          <Bold>Technicians: </Bold>
+          <Italic>
+            Alyssa Larson, Jae Starfox, Ryan Wolff, Dave Kaplan, Elise Liu, Lindsay Berkowitz, Dan Gerbang, Bryan
+            Bigler, Evin Wolverton, Hannah Schoonhoven, S. Caldwell
+          </Italic>
+          <br />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default AboutCredits;
